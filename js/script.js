643771721ea438afcd69e2cff4c74976e7662da5
”ukrWily” <ukrwily@gmail.com>
2024-01-26 21:06:04 +0200

    Added classes to class

--- js/script.js
+++ js/script.js
@@ -173,7 +173,7 @@ window.addEventListener('DOMContentLoaded', () => {
    * function
    * once show modal after 5s after load page
    */
-  // const modalTimerId = setTimeout(openModal, 5000);//todo: uncomment!!!
+  const modalTimerId = setTimeout(openModal, 5000);//todo: uncomment!!!
 
   /**
    * function
@@ -189,12 +189,13 @@ window.addEventListener('DOMContentLoaded', () => {
 
   // <{<{<{<{<{<{<{<{<{<{<{<    class for cards    >}>}>}>}>}>}>}>}>}>}>}>
   class MenuCard {
-    constructor(src, alt, title, descr, price, parentSelector){
+    constructor(src, alt, title, descr, price, parentSelector, ...classes){
       this.src = src;
       this.alt = alt;
       this.title = title;
       this.descr = descr;
       this.price = price;
+      this.classes = classes;
       this.parent = document.querySelector(parentSelector);
 
       this.transfer = 39;
@@ -207,17 +208,22 @@ window.addEventListener('DOMContentLoaded', () => {
 
     render() {
       let element = document.createElement('div');
+      if (this.classes.length === 0) {
+        this.element = 'menu__item';
+        element.classList.add(this.element);
+      } else {
+        this.classes.forEach(className => element.classList.add(className));
+      }
+
       element.innerHTML = `
-          <div class="menu__item">
-            <img src=${this.src} alt=${this.alt}>
-            <h3 class="menu__item-subtitle">${this.title}</h3>
-            <div class="menu__item-descr">${this.descr}</div>
-            <div class="menu__item-divider"></div>
-            <div class="menu__item-price">
-                <div class="menu__item-cost">Цена:</div>
-                <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
-            </div>
-        </div>
+          <img src=${this.src} alt=${this.alt}>
+          <h3 class="menu__item-subtitle">${this.title}</h3>
+          <div class="menu__item-descr">${this.descr}</div>
+          <div class="menu__item-divider"></div>
+          <div class="menu__item-price">
+              <div class="menu__item-cost">Цена:</div>
+              <div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+          </div>
       `;
       this.parent.append(element);
     }
@@ -229,7 +235,9 @@ window.addEventListener('DOMContentLoaded', () => {
     'Меню "Фитнес"',
     'Меню "Фитнес" - это новый подход к приготовлению блюд: больше свежих овощей и фруктов. Продукт активных и здоровых людей. Это абсолютно новый продукт с оптимальной ценой и высоким качеством!',
     9,
-    '.menu .container'
+    '.menu .container',
+    'menu__item',
+    'big'
   ).render();
 
   new MenuCard(
@@ -251,3 +259,4 @@ window.addEventListener('DOMContentLoaded', () => {
   ).render();
 
 });
+
