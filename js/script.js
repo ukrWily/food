6fb70cd71273412c845f47d176b2ebfc8a83fe86
”ukrWily” <ukrwily@gmail.com>
2024-01-29 21:37:30 +0200

    Added form processing

--- js/script.js
+++ js/script.js
@@ -258,5 +258,48 @@ window.addEventListener('DOMContentLoaded', () => {
     '.menu .container'
   ).render();
 
+  // <{<{<{<{<{<{<{<{<{<{<{<    Forms    >}>}>}>}>}>}>}>}>}>}>}>
+
+  const forms = document.querySelectorAll('form');
+
+  const message = {
+    loading: 'Loading',
+    success: "Ok. We'll call you",
+    failure: 'Something wrong...'
+  }
+
+  forms.forEach(item => {
+    postData(item);
+  })
+
+  function postData(form) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+
+      const statusMessage = document.createElement('div');
+      statusMessage.classList.add('status');
+      statusMessage.textContent = message.loading;
+      form.append(statusMessage);
+
+      const request = new XMLHttpRequest();
+      request.open('POST', 'server.php');
+
+      request.setRequestHeader('Content-type', 'multipart/form-data');
+      const formData = new FormData(form);
+
+      request.send(formData);
+
+      request.addEventListener('load', () => {
+        if (request.status === 200) {
+          console.log(request.response);
+          statusMessage.textContent = message.success;
+        } else {
+          statusMessage.textContent = message.failure;
+        }
+      });
+    });
+  }
+
+
 });
 
