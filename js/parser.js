9049ddaac849609300a26ad68bc7e12ce4ce3327
ukrWily <ukrwily@gmail.com>
2024-02-21 11:49:36 +0200

    Ended simple slider

--- js/parser.js
+++ js/parser.js
@@ -1,43 +1,43 @@
 // <{<{<{<{<{<{<{<{<{<{<{<    Parser    >}>}>}>}>}>}>}>}>}>}>}>
-//todo:  for insert to page's devtools - take without - window.addEventListener('DOMContentLoaded', () => {
+// //todo:  for insert to page's devtools - take without - window.addEventListener('DOMContentLoaded', () => {
 
-window.addEventListener('DOMContentLoaded', () => {
+// window.addEventListener('DOMContentLoaded', () => {
 
-  const body = document.querySelector('body');
-  let textNodes = [];
+//   const body = document.querySelector('body');
+//   let textNodes = [];
 
-  // <{<{<{<{<{<{<{<{<{<{<{<    parsing & writting to array    >}>}>}>}>}>}>}>}>}>}>}>
-  function recursy(element) {
-    element.childNodes.forEach(node => {
+//   // <{<{<{<{<{<{<{<{<{<{<{<    parsing & writting to array    >}>}>}>}>}>}>}>}>}>}>}>
+//   function recursy(element) {
+//     element.childNodes.forEach(node => {
 
-      if (node.nodeName.match(/^H\d/)) {        //todo: string starts(^) with big letter(H) than(\) go number(d) 
-        const obj = {
-          header: node.nodeName,
-          content: node.textContent.trim()
-        };
-        textNodes.push(obj);
-      } else {
-        recursy(node);
-      }
+//       if (node.nodeName.match(/^H\d/)) {        //todo: string starts(^) with big letter(H) than(\) go number(d) 
+//         const obj = {
+//           header: node.nodeName,
+//           content: node.textContent.trim()
+//         };
+//         textNodes.push(obj);
+//       } else {
+//         recursy(node);
+//       }
 
-    });
-  }
-  recursy(body);
+//     });
+//   }
+//   recursy(body);
   
   
 
-// <{<{<{<{<{<{<{<{<{<{<{<    sending to server    >}>}>}>}>}>}>}>}>}>}>}>
-  fetch('https://jsonplaceholder.typicode.com/posts', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify(textNodes)
-  })
-  .then(response => response.json())
-  .then(json => console.log(json));
+// // <{<{<{<{<{<{<{<{<{<{<{<    sending to server    >}>}>}>}>}>}>}>}>}>}>}>
+//   fetch('https://jsonplaceholder.typicode.com/posts', {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify(textNodes)
+//   })
+//   .then(response => response.json())
+//   .then(json => console.log(json));
   
-})
+// })
 
 
 
