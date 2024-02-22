20a21a641c7fbe602d0d062b9c338115e9247764
”ukrWily” <ukrwily@gmail.com>
2024-02-22 20:24:57 +0200

    Added functional to slider

--- js/script.js
+++ js/script.js
@@ -323,7 +323,8 @@ window.addEventListener('DOMContentLoaded', function() {
 
     slider.style.position = 'relative';
 
-    const indicators = document.createElement('ol');
+    const indicators = document.createElement('ol'),
+        dots = [];
     indicators.classList.add('carousel-indicators');
     indicators.style.cssText = `
         position: absolute;
@@ -340,7 +341,28 @@ window.addEventListener('DOMContentLoaded', function() {
     slider.append(indicators);
 
     for (let i = 0; i < slides.length; i++) {
-        
+        const dot = this.document.createElement('li');
+        dot.setAttribute('data-slide-to', i + 1);
+        dot.style.cssText = `
+            box-sizing: content-box;
+            flex: 0 1 auto;
+            width: 30px;
+            height: 6px;
+            margin-right: 3px;
+            margin-left: 3px;
+            cursor: pointer;
+            background-color: #fff;
+            background-clip: padding-box;
+            border-top: 10px solid transparent;
+            border-bottom: 10px solid transparent;
+            opacity: .5;
+            transition: opacity .6s ease;
+        `;
+        if (i == 0) {
+            dot.style.opacity = 1;
+        }
+        indicators.append(dot);
+        dots.push(dot);
     }
 
     next.addEventListener('click', () => {
